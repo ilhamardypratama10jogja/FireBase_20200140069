6f01e80d43715de67fdf4ec291f6b5596006c326
ilhamardypratama10jogja <ilhamardy10@gmail.com>
2022-06-11 18:15:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/firebase_todoapp/DetailActivity.java
@@ -0,0 +1,39 @@
+package com.example.firebase_todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private TextView nama,email;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        nama = findViewById(R.id.nama);
+        email = findViewById(R.id.txtEmail);
+
+        progressDialog = new ProgressDialog(DetailActivity.this);
+        progressDialog.setTitle("Loading");
+        progressDialog.setMessage("Mengambil data...");
+
+        Intent intent = getIntent();
+        if(intent != null){
+            nama.setText(intent.getStringExtra("name"));
+            email.setText(intent.getStringExtra("email"));
+        }
+
+
+    }
+}
